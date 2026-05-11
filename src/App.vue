332ca01973a7cdefaa96f626b029e2bc4605a56d
Chegoure <timildmitri4@icloud.com>
2026-05-11 19:20:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="m-auto mt-14 h-screen w-4/5 rounded-xl bg-white shadow-xl">
+    <Header />
+  </div>
+</template>
