44210b49bb02c214cabe6cec5d05aa9005706c7d
Chegoure <timildmitri4@icloud.com>
2026-06-21 12:45:30 +0300

    Реализовал сохранение корзины и отображение пустого состояния.

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
 import CartItemList from './CartItemList.vue'
+import InfoBlock from './InfoBlock.vue'
 
 const emit = defineEmits('createOrder')
 
@@ -15,28 +16,39 @@ const props = defineProps({
   <div class="fixed top-0 left-0 z-10 h-full w-full bg-black opacity-70"></div>
   <div class="fixed top-0 right-0 z-20 h-full w-96 bg-white p-8">
     <DrawerHead />
-    <CartItemList />
 
-    <div class="mt-7 flex flex-col gap-4">
-      <div class="flex gap-2">
-        <span>Итого:</span>
-        <div class="flex-1 border-b border-dashed border-[#DFDFDF]"></div>
-        <b>{{ totalPrice }} руб.</b>
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+        image-url="/package-icon.png"
+      />
+    </div>
+
+    <div v-else>
+      <CartItemList />
+
+      <div class="mt-7 flex flex-col gap-4">
+        <div class="flex gap-2">
+          <span>Итого:</span>
+          <div class="flex-1 border-b border-dashed border-[#DFDFDF]"></div>
+          <b>{{ totalPrice }} руб.</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span>Налог 5%:</span>
+          <div class="flex-1 border-b border-dashed border-[#DFDFDF]"></div>
+          <b>{{ vatPrice }} руб.</b>
+        </div>
+
+        <button
+          @click="emit('createOrder')"
+          :disabled="buttonDisabled"
+          class="mt-4 w-full cursor-pointer rounded-2xl bg-lime-500 py-3 text-white transition hover:bg-lime-600 active:bg-lime-700 disabled:bg-gray-300"
+        >
+          Оформить заказ
+        </button>
       </div>
-
-      <div class="flex gap-2">
-        <span>Налог 5%:</span>
-        <div class="flex-1 border-b border-dashed border-[#DFDFDF]"></div>
-        <b>{{ vatPrice }} руб.</b>
-      </div>
-
-      <button
-        @click="emit('createOrder')"
-        :disabled="buttonDisabled"
-        class="mt-4 w-full cursor-pointer rounded-2xl bg-lime-500 py-3 text-white transition hover:bg-lime-600 active:bg-lime-700 disabled:bg-gray-300"
-      >
-        Оформить заказ
-      </button>
     </div>
   </div>
 </template>
