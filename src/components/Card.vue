431f82e51ab183ce54522a3386a15543f6d34c9d
Chegoure <timildmitri4@icloud.com>
2026-06-25 17:03:17 +0300

    Исправил расположение элементов в карточке товара.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,7 +13,7 @@ defineProps({
 
 <template>
   <div
-    class="relative cursor-pointer rounded-[40px] border border-[#F3F3F3] bg-white p-8 transition hover:-translate-y-2 hover:shadow-xl"
+    class="relative flex h-full cursor-pointer flex-col rounded-[40px] border border-[#f3f3f3] bg-white p-8 transition hover:-translate-y-2 hover:shadow-xl"
   >
     <img
       v-if="onClickFavorite"
@@ -23,12 +23,15 @@ defineProps({
       @click="onClickFavorite"
     />
 
-    <img :src="imageUrl" alt="Sneakers" />
-    <p class="mt-2">{{ title }}</p>
+    <div class="flex h-52 w-full items-center justify-center">
+      <img :src="imageUrl" alt="Sneakers" class="h-full object-contain" />
+    </div>
 
-    <div class="mt-5 flex justify-between">
+    <p class="mt-2 text-slate-700">{{ title }}</p>
+
+    <div class="mt-auto flex items-end justify-between pt-5">
       <div class="flex flex-col">
-        <span class="text-[#BDBDBD] uppercase">Цена:</span>
+        <span class="text-xs text-[#BDBDBD] uppercase">Цена:</span>
         <b>{{ price }} руб.</b>
       </div>
       <img
