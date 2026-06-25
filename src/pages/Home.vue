8443bbf5a71d7da86ee133192be82737be29d7da
Chegoure <timildmitri4@icloud.com>
2026-06-25 11:38:14 +0300

    Добавил debounce для поиска товаров.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { inject, reactive, watch, ref, onMounted } from 'vue'
 import CardList from '../components/CardList.vue'
+import debounce from 'lodash.debounce'
 
 const { cart, addToCart, removeFromCart } = inject('cart')
 
@@ -25,9 +26,9 @@ const onChangeSelect = (event) => {
   filters.sortBy = event.target.value
 }
 
-const onChangeSearchInput = (event) => {
+const onChangeSearchInput = debounce((event) => {
   filters.searchQuery = event.target.value
-}
+}, 500)
 
 const addToFavorite = async (item) => {
   try {
